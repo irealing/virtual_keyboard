5b2b8231eda8bbc96050943a91b1ad73f6749fb6
Memory_Leak <yuz@cnns.net>
2018-03-14 11:14:50 +0800

    feat: cgo不在输出按键信息

--- keyboard/keyboard.c
+++ keyboard/keyboard.c
@@ -3,7 +3,5 @@
 int key_event(unsigned char code,short up){
     int flag=up?2:0;
     keybd_event(code,0,flag,0);
-    char *action=up?RELEASE:PRESS;
-    printf("Cgo: %s key %d \n",action,code);
     return flag;
 }
--- keyboard/keyboard.h
+++ keyboard/keyboard.h
@@ -3,10 +3,6 @@
 #include <windows.h>
 #include <stdio.h>
 
-#define PRESS "PRESS"
-
-#define RELEASE "RELEASE"
-
 int key_event(unsigned char code,short up);
 
 #endif
